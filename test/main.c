26fe86b077572b2b1df478ee048d4a228bd55d25
fbonhomm <flo-github@outlook.fr>
2019-05-31 16:17:55 +0200

    feat(test): add tes directory with one file for test

---
+++ test/main.c
@@ -0,0 +1,13 @@
+
+#include "ft_printf.h"
+
+int main() {
+    int i;
+
+    i = 42;
+    ft_printf("Hello World !\n");
+    ft_printf("%d\n", i);
+    ft_printf("%s\n", "42");
+
+    return (0);
+}
